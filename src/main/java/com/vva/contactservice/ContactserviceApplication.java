ed88580b69b7c89eff571ba5fd7111fee6d5d45e
derekgygax <derekgygax@gmail.com>
2024-09-29 16:58:26 -0400

    Initial commit for contactservice project

---
+++ src/main/java/com/vva/contactservice/ContactserviceApplication.java
@@ -0,0 +1,13 @@
+package com.vva.contactservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vva/contactservice/ContactserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vva.contactservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
